ce1dcf35afa9ebb52060884687ebe97bd1147649
hangxigood <hangxigood@gmail.com>
2026-01-07 16:55:18 -0700

    Update production API URL

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    // TODO: Replace with your actual Azure backend URL
+    apiUrl: 'https://xlsx-grid-flow-api-eqcxbdazhkd8e4hb.canadaeast-01.azurewebsites.net/api'
+};
